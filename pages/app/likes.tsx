3160693458959dc91ab9995c9b5357e386119c16
EnzoPoint <enzo.carpentier.pro@gmail.com>
2023-06-27 11:55:59 +0200

    V1

--- pages/app/likes.tsx
+++ pages/app/likes.tsx
@@ -1,20 +1,45 @@
 import SectionHeader from "@components/layout/section-header"
 import SectionTitle from "@components/section-title"
+import useDeviceType from "../../hooks/use-device-type";
+import { getSpots } from '@lib/helpers/spots'
+import { GetServerSideProps } from 'next'
+import { serialize, deserialize } from 'superjson'
+import { SuperJSONResult } from 'superjson/dist/types'
+import { useSession } from "next-auth/react";
+import { SessionUser } from "@lib/types";
+import LoginModal from "@components/modals/login-modal";
+import NotConnected from "@components/not-connected";
+import styles from "@styles/pages/likes.module.scss";
 
-
-
+interface Props {
+	spots: SuperJSONResult,
+	open: boolean;
+	id: string | string[] | undefined;
+}
 
 const Likes = () => {
 
-
+    const { data: session, status } = useSession()
+    const user = session?.user as SessionUser | undefined
+    const deviceType = useDeviceType();
+    
     // render
 
     return (
-        <main>
+        <main id={styles.main}>
             <SectionHeader>
                 <SectionTitle>J&apos;aimes</SectionTitle>
                 <p>Les spots qui vous ont tapé dans l&apos;œil</p>
             </SectionHeader>
+            
+
+            {   status !== "authenticated" &&
+                <>
+                    <div className={styles.authenticated}>
+                        <NotConnected />
+                    </div>
+                </>
+            }   
         </main>
     )
 }
