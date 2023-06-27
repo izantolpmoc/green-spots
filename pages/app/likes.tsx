fc3abb9f9099c50431071d939d83c83e7b68d3ff
EnzoPoint <enzo.carpentier.pro@gmail.com>
2023-06-27 15:44:06 +0200

    V1.3

--- pages/app/likes.tsx
+++ pages/app/likes.tsx
@@ -13,9 +13,11 @@ import { authOptions } from "@pages/api/auth/[...nextauth]";
 import SpotDetailsModal from '@components/modals/spot-details-modal'
 import SpotCard from '@components/spot-card'
 import { useState } from 'react'
+import { Spot } from '@lib/types'
+import prisma from "@lib/prisma";
 
 interface Props {
-	likedSpots: SuperJSONResult,
+	likedSpots: SuperJSONResult | null,
 }
 
 const Likes = (
@@ -25,9 +27,23 @@ const Likes = (
     const { data: session, status } = useSession()
     const deviceType = useDeviceType();
     const [showModal, setShowModal] = useState(false);
+    const [currentSpotPosition, setCurrentSpotPosition] = useState(0);
     
     const [data] = useState(likedSpots ? deserialize(likedSpots) : []);
 
+    const getCurrentSpotPosition = (id: number) => {
+        const spots = data as unknown as Spot[];
+
+        const currentSpotIdx = spots?.findIndex(spot => spot.id == spots[id].id);
+
+        return setCurrentSpotPosition(currentSpotIdx);
+	}
+
+    const openModal = (id: number) => {
+        getCurrentSpotPosition(id);
+        setShowModal(true);
+    }
+    
     // render
 
     return (
@@ -40,51 +56,59 @@ const Likes = (
 
             {   status !== "authenticated" ?
                 <>
-                    <div className={styles.authenticated}>
-                        <NotConnected />
-                    </div>
+                        <div className={styles.authenticated}>
+                            <NotConnected />
+                        </div>
                 </> :
                 <>
-                
-                {
-                    deviceType !== "mobile" ?
-                    <>
-                        <div className={styles.container}>
-                            {data.map((item, i) => {
-                                return (
-                                    <SpotCard
-                                        key={i}
-                                        displayMode='card'
-                                        spot={item}
-                                        onClick={() => setShowModal(true)}
-                                    />
-                                );
-                            })}
-                        </div>
-                    </>
-                    : 
+                    
+                    {
+                        deviceType !== "mobile" ?
+                        <>
+                          <div className={styles.container}>
+                                {data.map((item, i) => {
+                                    return (
+                                        <SpotCard
+                                            key={i}
+                                            displayMode='card'
+                                            spot={item}
+                                            onClick={() => openModal(i)}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </>
+                        : 
 
-                    <>
-                        <div className={styles.container}>
-                            {data.map((item, i) => {
-                                return (
-                                    <SpotCard
-                                        key={i}
-                                        displayMode='list'
-                                        spot={item}
-                                        onClick={() => setShowModal(true)}
-                                    />
-                                );
-                            })}
-                        </div>
-                    </>
-                }
+                        <>
+                            <div className={styles.container}>
+                                {data.map((item, i) => {
 
-                {/* <SpotDetailsModal showModal={showModal} setShowModal={setShowModal} spots={data} currentSpotPosition={currentSpotPosition} setCurrentSpotPosition={setCurrentSpotPosition}></SpotDetailsModal> */}
+                                    console.log(item)
+                                    return (
+                                        <SpotCard
+                                            key={i}
+                                            displayMode='list'
+                                            spot={item}
+                                            onClick={() => openModal(i)}
+                                        />
+                                    );
+                                })}
+                            </div>
+                        </>
+                    }
+
+                    <SpotDetailsModal
+                        showModal={showModal}
+                        setShowModal={setShowModal}
+                        spots={data}
+                        currentSpotPosition={currentSpotPosition}
+                        setCurrentSpotPosition={setCurrentSpotPosition}
+                    />
+
                 </>
             }   
-
-
+            
         </main>
     )
 }
@@ -93,17 +117,38 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
 
     const session = await getServerSession(context.req, context.res, authOptions)
 
+    if(!session) return {
+        props: {
+            likedSpots: null
+        }
+    }
+
     let email = session.user.email;
 
     const rawData = await getUser(email)
+
+    const likedSpots: any[] = []
+
+    for(const likedSpot of rawData.likedSpots) {
+
+        likedSpots.push(await prisma.spot.findUnique({
+            where: {
+                id: likedSpot.id
+            },
+            include: {
+                tags: true,
+                reviews: true
+            }
+        }))
+    }
       
-    const likedSpots = serialize(rawData.likedSpots)
+    const likedSpotsJSON = serialize(likedSpots)
     
     // return 
 
     return {
         props : {
-            likedSpots
+            likedSpots: likedSpotsJSON
         }
     }
 }
