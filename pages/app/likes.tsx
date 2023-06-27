2bae1a88f2211eea25ec843d0b7865c7f6d270a6
EnzoPoint <enzo.carpentier.pro@gmail.com>
2023-06-27 14:45:08 +0200

    V1.1

--- pages/app/likes.tsx
+++ pages/app/likes.tsx
@@ -1,28 +1,33 @@
 import SectionHeader from "@components/layout/section-header"
 import SectionTitle from "@components/section-title"
 import useDeviceType from "../../hooks/use-device-type";
-import { getSpots } from '@lib/helpers/spots'
+import { getUser } from '@lib/helpers/user'
 import { GetServerSideProps } from 'next'
 import { serialize, deserialize } from 'superjson'
 import { SuperJSONResult } from 'superjson/dist/types'
 import { useSession } from "next-auth/react";
-import { SessionUser } from "@lib/types";
-import LoginModal from "@components/modals/login-modal";
 import NotConnected from "@components/not-connected";
 import styles from "@styles/pages/likes.module.scss";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@pages/api/auth/[...nextauth]";
+import SpotDetailsModal from '@components/modals/spot-details-modal'
+import SpotCard from '@components/spot-card'
+import { useState } from 'react'
 
 interface Props {
-	spots: SuperJSONResult,
-	open: boolean;
-	id: string | string[] | undefined;
+	likedSpots: SuperJSONResult,
 }
 
-const Likes = () => {
+const Likes = (
+    { likedSpots }: Props
+) => {
 
     const { data: session, status } = useSession()
-    const user = session?.user as SessionUser | undefined
     const deviceType = useDeviceType();
+    const [showModal, setShowModal] = useState(false);
     
+    const [data] = useState(likedSpots ? deserialize(likedSpots) : []);
+
     // render
 
     return (
@@ -33,15 +38,74 @@ const Likes = () => {
             </SectionHeader>
             
 
-            {   status !== "authenticated" &&
+            {   status !== "authenticated" ?
                 <>
                     <div className={styles.authenticated}>
                         <NotConnected />
                     </div>
+                </> :
+                <>
+                
+                {
+                    deviceType !== "mobile" ?
+                    <>
+                        <div className={styles.container}>
+                            {data.map((item, i) => {
+                                return (
+                                    <SpotCard
+                                        key={i}
+                                        displayMode='card'
+                                        spot={item}
+                                        onClick={() => setShowModal(true)}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </>
+                    : 
+
+                    <>
+                        <div className={styles.container}>
+                            {data.map((item, i) => {
+                                return (
+                                    <SpotCard
+                                        key={i}
+                                        displayMode='list'
+                                        spot={item}
+                                        onClick={() => setShowModal(true)}
+                                    />
+                                );
+                            })}
+                        </div>
+                    </>
+                }
+
+                {/* <SpotDetailsModal showModal={showModal} setShowModal={setShowModal} spots={data} currentSpotPosition={currentSpotPosition} setCurrentSpotPosition={setCurrentSpotPosition}></SpotDetailsModal> */}
                 </>
             }   
+
+
         </main>
     )
+}
+
+export const getServerSideProps: GetServerSideProps<Props> = async (context) => {
+
+    const session = await getServerSession(context.req, context.res, authOptions)
+
+    let email = session.user.email;
+
+    const rawData = await getUser(email)
+      
+    const likedSpots = serialize(rawData.likedSpots)
+    
+    // return 
+
+    return {
+        props : {
+            likedSpots
+        }
+    }
 }
 
 export default Likes
