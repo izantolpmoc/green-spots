2dc5ce6c3dd9a77d92289f13d99cdf575a99be17
Naomie <naomie.discala@gmail.com>
2023-06-29 11:00:15 +0200

    removed unused elements and added include to db results

--- pages/app/likes.tsx
+++ pages/app/likes.tsx
@@ -1,6 +1,5 @@
 import SectionHeader from "@components/layout/section-header"
 import SectionTitle from "@components/section-title"
-import useDeviceType from "../../hooks/use-device-type";
 import { getUser } from '@lib/helpers/user'
 import { GetServerSideProps } from 'next'
 import { serialize, deserialize } from 'superjson'
@@ -9,8 +8,6 @@ import { useSession } from "next-auth/react";
 import styles from "@styles/pages/likes.module.scss";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@pages/api/auth/[...nextauth]";
-import SpotDetailsModal from '@components/modals/spot-details-modal'
-import SpotCard from '@components/spot-card'
 import { useState } from 'react'
 import { Spot } from '@lib/types'
 import prisma from "@lib/prisma";
@@ -29,21 +26,8 @@ const Likes = (
 ) => {
 
     const { status } = useSession()
-    const deviceType = useDeviceType()
-    const [showModal, setShowModal] = useState(false)
-    const [currentSpotPosition, setCurrentSpotPosition] = useState(0)
     
     const [likedSpots] = useState<Spot[]>(likedSpotsJSON ? deserialize(likedSpotsJSON) : [])
-
-    const getCurrentSpotPosition = (id: number) => {
-        const currentSpotIdx = likedSpots.findIndex(spot => spot.id == likedSpots[id].id)
-        return setCurrentSpotPosition(currentSpotIdx)
-	}
-
-    const openModal = (id: number) => {
-        getCurrentSpotPosition(id)
-        setShowModal(true)
-    }
 
     // manage login modal
 
@@ -130,14 +114,16 @@ export const getServerSideProps: GetServerSideProps<Props> = async (context) => 
             },
             include: {
                 tags: true,
-                reviews: true,
+                reviews: {
+                    include: { user: true }
+                },
                 likedBy: true
             }
         }))
     }
 
     // serialize the liked spots to send them to the client
-      
+
     const likedSpotsJSON = serialize(likedSpots)
     
     // return 
